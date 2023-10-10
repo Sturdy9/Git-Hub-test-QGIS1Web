614dacbe54b1baca8175dc7903072d81c99382e3
Sturdy9 <mattsturdy@hotmail.co.uk>
2023-10-10 22:25:11 +1100

    Initial commit

---
+++ mapbox/style.js
@@ -0,0 +1,181 @@
+
+var styleJSON = {
+    "version": 8,
+    "name": "qgis2web export",
+    "pitch": 0,
+    "light": {
+        "intensity": 0.2
+    },
+    "sources": {
+        "Area1GRB_0": {
+            "type": "geojson",
+            "data": json_Area1GRB_0
+        }
+                    ,
+        "Area1Cont_1": {
+            "type": "geojson",
+            "data": json_Area1Cont_1
+        }
+                    ,
+        "Area2GRB_2": {
+            "type": "geojson",
+            "data": json_Area2GRB_2
+        }
+                    ,
+        "Area2Cont_3": {
+            "type": "geojson",
+            "data": json_Area2Cont_3
+        }
+                    ,
+        "Area2_4": {
+            "type": "geojson",
+            "data": json_Area2_4
+        }
+                    ,
+        "Area3GRB_5": {
+            "type": "geojson",
+            "data": json_Area3GRB_5
+        }
+                    ,
+        "Area3Cont_6": {
+            "type": "geojson",
+            "data": json_Area3Cont_6
+        }
+                    ,
+        "Area3_7": {
+            "type": "geojson",
+            "data": json_Area3_7
+        }
+                    ,
+        "Area4GRB_8": {
+            "type": "geojson",
+            "data": json_Area4GRB_8
+        }
+                    ,
+        "Area4Cont_9": {
+            "type": "geojson",
+            "data": json_Area4Cont_9
+        }
+                    ,
+        "Area4_10": {
+            "type": "geojson",
+            "data": json_Area4_10
+        }
+                    ,
+        "Capture_modified_11": {
+            "type": "image",
+            "url": "./data/Capture_modified_11.png",
+            "coordinates": [
+                [150.317157, -32.900633],
+                [151.678685, -32.900633],
+                [151.678685, -31.830715],
+                [150.317157, -31.830715]
+            ]
+        }},
+    "sprite": "",
+    "glyphs": "https://glfonts.lukasmartinelli.ch/fonts/{fontstack}/{range}.pbf",
+    "layers": [
+        {
+            "id": "background",
+            "type": "background",
+            "layout": {},
+            "paint": {
+                "background-color": "#ffffff"
+            }
+        },
+        {
+            "id": "lyr_Area1GRB_0_0",
+            "type": "fill",
+            "source": "Area1GRB_0",
+            "layout": {},
+            "paint": {'fill-opacity': 1.0, 'fill-color': '#7d8b8f'}
+        }
+,
+        {
+            "id": "lyr_Area1Cont_1_0",
+            "type": "fill",
+            "source": "Area1Cont_1",
+            "layout": {},
+            "paint": {'fill-opacity': 1.0, 'fill-color': '#ffe711'}
+        }
+,
+        {
+            "id": "lyr_Area2GRB_2_0",
+            "type": "fill",
+            "source": "Area2GRB_2",
+            "layout": {},
+            "paint": {'fill-opacity': 0.5, 'fill-color': '#e41a1c'}
+        }
+,
+        {
+            "id": "lyr_Area2Cont_3_0",
+            "type": "fill",
+            "source": "Area2Cont_3",
+            "layout": {},
+            "paint": {'fill-opacity': 0.5, 'fill-color': '#ffe711'}
+        }
+,
+        {
+            "id": "lyr_Area2_4_0",
+            "type": "fill",
+            "source": "Area2_4",
+            "layout": {},
+            "paint": {'fill-opacity': 0.5, 'fill-color': '#4daf4a'}
+        }
+,
+        {
+            "id": "lyr_Area3GRB_5_0",
+            "type": "fill",
+            "source": "Area3GRB_5",
+            "layout": {},
+            "paint": {'fill-opacity': 0.5, 'fill-color': '#e41a1c'}
+        }
+,
+        {
+            "id": "lyr_Area3Cont_6_0",
+            "type": "fill",
+            "source": "Area3Cont_6",
+            "layout": {},
+            "paint": {'fill-opacity': 0.5, 'fill-color': '#ffe711'}
+        }
+,
+        {
+            "id": "lyr_Area3_7_0",
+            "type": "fill",
+            "source": "Area3_7",
+            "layout": {},
+            "paint": {'fill-opacity': 0.5, 'fill-color': '#4daf4a'}
+        }
+,
+        {
+            "id": "lyr_Area4GRB_8_0",
+            "type": "fill",
+            "source": "Area4GRB_8",
+            "layout": {},
+            "paint": {'fill-opacity': 0.5, 'fill-color': '#e41a1c'}
+        }
+,
+        {
+            "id": "lyr_Area4Cont_9_0",
+            "type": "fill",
+            "source": "Area4Cont_9",
+            "layout": {},
+            "paint": {'fill-opacity': 0.5, 'fill-color': '#ffe711'}
+        }
+,
+        {
+            "id": "lyr_Area4_10_0",
+            "type": "fill",
+            "source": "Area4_10",
+            "layout": {},
+            "paint": {'fill-opacity': 0.5, 'fill-color': '#4daf4a'}
+        }
+,
+        {
+            "id": "lyr_Capture_modified_11_11",
+            "type": "raster",
+            "source": "Capture_modified_11",
+            "minzoom": 0,
+            "maxzoom": 22
+        }],
+}
